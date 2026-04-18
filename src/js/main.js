26757f4b498f51d4d46c0e9484a3ab937885824c
Andrew <wotriger@gmail.com>
2026-04-18 22:36:30 +0200

    created section dashboard & words

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function showTabContent(i = 0) {
-    tabsContent[i].style.display = 'block'
+    tabsContent[i].style.display = 'grid'
     tabs[i].classList.add('sidebar__item-active')
   }
 
